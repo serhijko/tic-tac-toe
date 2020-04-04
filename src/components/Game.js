9a673ec19fd453ce7705aff79c1517485c1c668a
Serhij Korneluk <serhij@debian.home>
2020-04-04 08:36:34 +0300

    fix square already fill

--- src/components/Game.js
+++ src/components/Game.js
@@ -20,7 +20,7 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    if (calculateWinner(squares).winner) {
+    if (calculateWinner(squares).winner || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? '❌' : '⭕';
